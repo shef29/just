848905319a8355c1240e08780f1fe6859759f5b9
Pento <pentowagner@gmail.com>
2018-12-30 21:02:20 +0200

    mod footer

--- publish/resources/views/admin/layouts/footer.blade.php
+++ publish/resources/views/admin/layouts/footer.blade.php
@@ -14,8 +14,6 @@
             <script>
                 document.write(new Date().getFullYear())
             </script>
-            , made with <i class="material-icons">favorite</i> by
-            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
         </div>
         <!-- your footer here -->
     </div>
